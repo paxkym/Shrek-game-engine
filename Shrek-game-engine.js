2ebab30486e1af2fcfe37fda1dbfe447f26ae387
paxkym <104684664+paxkym@users.noreply.github.com>
2022-11-22 01:15:12 -0500

    Added (full) sprites

--- Shrek-game-engine.js
+++ Shrek-game-engine.js
@@ -1,14 +1,33 @@
-var game = { loop:function(){}, 
+var game = { 
+    loop:function(){}, 
 maps:[],
-settings:{frameRate:25, antiAliasing:false,
+settings:{
+    frameRate:25,
+     antiAliasing:false,
      dialogue:{font:'Helvetica', size:'10px', color:'black', background:'teal'}
     },
  target:null, 
  map:null, 
  room:null, 
- break:false 
+ break:false,
+ windowWidth:450,
+ windowHeight:200
 }
-class item {
+function loadImage(path, x, y){
+    if(game.settings.antiAliasing){
+        game.target.imageSmoothingEnabled = true
+    }else{
+        game.target.imageSmoothingEnabled = false
+    }
+    var ld = new Image()
+    ld.src = path
+    ld.onload = function(){
+game.target.drawImage(ld, x, y)
+    }
+}
+class Item {
+width = 10
+height = 10
 x = 0
 y = 0
 track = []
@@ -25,31 +44,57 @@ this.track = track
 }
 render(){
     this.frame++
-    if(this.frame>this.frames){
-        i++
+    if(this.frame == this.frames+1){
+        this.i++
         this.frame = 0
     }
-    console.log(this.i, this.frame, this.track.length, this.frames)
-    if(this.i>this.track.length){
+    if(this.i == this.track.length){
         this.i = 0
     }
-    console.log(this.x)
-loadImage(this.track[i],this.x,this.y)
+    // console.log(this.track[this.i])
+loadImage(this.track[this.i],this.x,this.y)
 }
 }
-function loadImage(path, x, y){
-    var ld = new Image()
-    if(game.settings.antiAliasing){
-        game.target.imageSmoothingEnabled = true
-    }else{
-        game.target.imageSmoothingEnabled = false
+class Sprite {
+    width = 10
+    height = 10
+    x = 0
+    y = 0
+    tracks = []
+    track = 0
+    collision = {
+    on: function(){},
+    type:null,
+    number:null
     }
-    ld.src = path
-    ld.onload = function(){
-game.target.drawImage(ld, x, y)
+    p = {}
+    frames = 5
+    i = 0
+    frame = 0
+    stop = true
+    constructor(x, y, tracks, frames){
+        this.frames = frames
+        this.i = 0
+        this.frame = 0
+        this.x = x
+    this.y = y
+    this.tracks = tracks
     }
-}
-
+    render(){
+        if(!this.stop){
+        this.frame++
+        if(this.frame == this.frames+1){
+            this.i++
+            this.frame = 0
+        }
+        if(this.i == this.tracks[this.track].length){
+            this.i = 0
+        }
+    }
+        // console.log(this.track[this.i])
+    loadImage(this.tracks[this.track][this.i],this.x,this.y)
+    }
+    }
 function wait(time){
     setTimeout(start, time*1000)
 }
@@ -57,21 +102,7 @@ function start(){
     if(game.break){
         return
     }
-    wait(1/game.settings.frameRate)
     game.loop()
+    wait(1/game.settings.frameRate)
+    game.target.clearRect(0, 0, game.windowWidth, game.windowHeight);
 }
-//Game starts here. Everything above is the library
-const canvas = document.getElementById('game')
-const ctx = canvas.getContext('2d')
-game.target = ctx
-var i = 0
-const Graphic = new item(10,10,['shrek/Shrek-backward-1.png','shrek/Shrek-backward-2.png','shrek/Shrek-backward-3.png'], 5)
-
-console.log(Graphic.u)
-game.loop = function(){
-    i++
-    console.log('amogus')
-    game.break = i>100
-    Graphic.render()
-}
-start()
